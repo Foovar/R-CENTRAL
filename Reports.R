80a6c817c97f27819012b7de5244231baf2d7e0d
Alex <aparedese@upao.edu.pe>
2017-07-13 20:17:20 -0500

    Primer reporte

--- Reports.R
+++ Reports.R
@@ -2,41 +2,27 @@ setwd("/Users/Alex/Programacion/R/PROYECTO")
 library(dplyr)
 library(highcharter)
 source("./scripts/group_sum.R")
+library(zoo)
+library(xts)
+library(dygraphs)
 
 log <- read.csv("input/Master.csv")
 colnames(log) <- list( "src", "dst", "dcontext", "clid", "channel", "dstchannel", "lastapp", "lastdata", "start", "answer", "end", "duration", "billsec", "disposition", "amaflags", "uniqueid" )
 callfailed <- log[log$answer == "" & log$dst != "123" , ]
 callsupport <- log[log$dst == "123", ]
 
-data <-  group_sum(callfailed)
-
-hc <- highchart() %>% 
- hc_xAxis(categories = data$fecha) %>% 
- hc_add_series(name = "Busy", data = data$busy) %>% 
- hc_add_series(name = "Congestion", data = data$congestion) %>% 
- hc_add_series(name = "No Answer",data = (data$no_answer)/2)
-
-hc
+xx <- hours(callfailed)
+tsdata = xts(x = xx[,-1], order.by = strptime(xx[,1], format='%Y-%m-%d %H'))
 
 
+dygraph(tsdata[,1], main = "All", group = "lung-deaths")
+dygraph(tsdata[,2], main = "Male", group = "lung-deaths")
+dygraph(tsdata[,3], main = "Female", group = "lung-deaths")
 
-''' library("highcharter")
-library("quantmod")
 
-usdjpy <- getSymbols("USD/JPY", src = "oanda", auto.assign = FALSE)
-eurkpw <- getSymbols("EUR/KPW", src = "oanda", auto.assign = FALSE)
+dygraph(tsdata, main = "Reporte. Llamadas sin exito.") %>%
+  dyOptions( drawPoints = T, pointSize = 2, colors = RColorBrewer::brewer.pal(3, "Set2")) %>%
+  dyRangeSelector(dateWindow = c(as.character( xx$fecha[length(xx$fecha) - 168] ),as.character(tail(xx[,1],1))))
 
-hc <- highchart(type = "stock") %>% 
-  hc_title(text = "Charting some Symbols") %>% 
-  hc_subtitle(text = "Data extracted using quantmod package") %>% 
-  hc_add_series(usdjpy, id = "usdjpy") %>% 
-  hc_add_series(eurkpw, id = "eurkpw")
-
-hc
-
-hc <- highchart() %>% 
-  hc_xAxis(categories = citytemp$month) %>% 
-  hc_add_series(name = "Tokyo", data = callfailed$disposition) %>% 
-  hc_add_series(name = "London", data = citytemp$london) %>% 
-  hc_add_series(name = "Other city",data = (citytemp$tokyo + citytemp$london)/2) '''
-
+#  dyHighlight(hideOnMouseOut = FALSE, highlightSeriesBackgroundAlpha = 0.2) %>%
+ts(1:10, frequency = 4, start = c(1959, 2))
